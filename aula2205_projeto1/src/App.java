3f8e2489d7a28bef675056e043fb8759e58dd3f8
Gustavo Aparecido da Silva <139185842+guch4n@users.noreply.github.com>
2026-05-24 19:34:36 -0300

    Correção e conclusão do primeiro desafio

--- aula2205_projeto1/src/App.java
+++ aula2205_projeto1/src/App.java
@@ -1,95 +1,131 @@
 import java.util.Scanner;
 
 // ESTÁ COM BUG NOS QUARTOS, É PRECISO VERIFICAR O QUARTO 0 (possivel solução, quarto - 1)
+// Possiveis melhorias:
+// Verificar os bugs do applicativo
 public class App {
     public static void main(String[] args) throws Exception {
-        double faturamentoTotal;
-        int numeroQuarto = 0, quartoDesejado;
+        String nomeHospede;
+        double faturamentoTotal = 0;
+        int numeroQuarto = 0, quartoDesejado, opcao;
         Scanner teclado = new Scanner(System.in);
         Quarto[] quartos = new Quarto[10];
 
-        // NOMEIA OS QUARTOS E MOSTRA ------------------
+        // CRIA E NOMEIA OS QUARTOS ------------------
         for (int i = 0; i < 10; i++) {
             if (i < 5) {
                 numeroQuarto = numeroQuarto + 1;
                 quartos[i] = new Quarto(numeroQuarto, "Simples");
-                System.out.println(quartos[i].getNumero() + " " + quartos[i].getTipo());
             } else {
                 numeroQuarto = numeroQuarto + 1;
                 quartos[i] = new Quarto(numeroQuarto, "Luxo");
-                System.out.println(quartos[i].getNumero() + " " + quartos[i].getTipo());
             }
 
         }
 
-        // MAPA DE QUARTOS -----------------------------
-        for (Quarto i : quartos) {
-            if (i.getOcupado()) {
-                String quartoOcupado = "Ocupado";
-                System.out.println("O Quarto " + i.getNumero() +
-                        " está " + quartoOcupado + "pelo Hospede: " + i.getHospede());
-            } else {
-                String quartoOcupado = "Livre";
-                System.out.println("O Quarto " + i.getNumero() +
-                        " está " + quartoOcupado);
-            }
+        do {
+            // Menu inicial ---------------------------------------
+            System.out.println("Seja bem vindo ao menu principal do ReservaDeHoteis.com, escolha a opção desejada: ");
+            System.out.println("(1) - Mostrar o mapa de quartos;");
+            System.out.println("(2) - Fazer Check-in;");
+            System.out.println("(3) - Fazer Check-out;");
+            System.out.println("(4) - Buscar hospede;");
+            System.out.println("(0) - Encerrar expediente e mostrar faturamento.");
+            System.out.println(" ");
+            System.out.println("Digite a opção desejada: ");
 
-        }
+            opcao = teclado.nextInt();
 
-        // Check-in -----------------------------------
-        System.out.println("Informe o numero do quarto que deseja: ");
-        quartoDesejado = teclado.nextInt();
+            switch (opcao) {
 
-        if (!quartos[quartoDesejado].getOcupado()) {
-            teclado.nextLine();
-            String nome, cpf, telefone;
-            int dias;
-            System.out.println("O Quarto encontra-se Livre, por favor me informe seu nome:");
-            nome = teclado.nextLine();
-            System.out.println("Agora seu CPF:");
-            cpf = teclado.nextLine();
-            System.out.println("Seu telefone:");
-            telefone = teclado.nextLine();
-            System.out.println("Agora me informe quantos dias de hospedagem você deseja:");
-            dias = teclado.nextInt();
-            teclado.nextLine();
+                case 1:
+                    // MAPA DE QUARTOS -----------------------------
+                    do{
+                        for (Quarto i : quartos) {
+                            if (i.getOcupado()) {
+                            String quartoOcupado = "Ocupado";
+                            System.out.println("O Quarto " + i.getTipo() + " " + i.getNumero() +
+                                    " está " + quartoOcupado + " pelo Hospede: " + i.getHospede().getNome());
+                            } else {
+                            String quartoOcupado = "Livre";
+                            System.out.println("O Quarto " + i.getTipo() + " " + i.getNumero() +
+                                    " está " + quartoOcupado);
+                            }
+                        }
+                        System.out.println("");
+                    } while (opcao != 1);
+                    System.out.println("");
+                    teclado.nextLine();                     
+                    break;
 
-            Hospede novoHospede = new Hospede(nome, cpf, telefone);
-            quartos[quartoDesejado].checkIn(novoHospede, dias);
+                case 2:
+                    // Check-in -----------------------------------
+                    System.out.println("Informe o numero do quarto que deseja fazer check-in: ");
+                    quartoDesejado = teclado.nextInt() - 1;
 
-        }
+                    if (!quartos[quartoDesejado].getOcupado()) {
+                        teclado.nextLine();
+                        String nome, cpf, telefone;
+                        int dias;
+                        System.out.println("O Quarto encontra-se Livre, por favor me informe seu nome:");
+                        nome = teclado.nextLine();
+                        System.out.println("Agora seu CPF:");
+                        cpf = teclado.nextLine();
+                        System.out.println("Seu telefone:");
+                        telefone = teclado.nextLine();
+                        System.out.println("Agora me informe quantos dias de hospedagem você deseja:");
+                        dias = teclado.nextInt();
 
-        // Check-out ----------------------------------
-        /*
-         * double calculoTotal;
-         * System.out.
-         * println("Me informe o numero do quarto que deseja fazer o checkout: ");
-         * quartoDesejado = teclado.nextInt();
-         * System.out.print("O preço da fatura ficou em: R$");
-         * System.out.println(calculoTotal = quartos[quartoDesejado].checkOut());
-         */
-        // ---------------------------------------------
+                        Hospede novoHospede = new Hospede(nome, cpf, telefone);
+                        quartos[quartoDesejado].checkIn(novoHospede, dias);
+                        System.out.println("");
+                    } else {
+                        System.out.println("Quarto já ocupado, tente realizar o check in em outro quarto.\n");
+                    }
+                    teclado.nextLine();
+                    break;
+            
+                case 3:
+                    // Check-out ----------------------------------                    
+                    double calculoTotal;
+                    System.out.println("Me informe o numero do quarto que deseja fazer o checkout: ");
+                    quartoDesejado = teclado.nextInt() - 1; // o correto é fazer uma função que execute isso
+                    System.out.print("O preço da fatura ficou em: R$");
+                    System.out.println(calculoTotal = quartos[quartoDesejado].checkOut());
+                    System.out.println("");
+                    faturamentoTotal = faturamentoTotal + calculoTotal;
+                    teclado.nextLine();
+                    break;
+                
+                case 4:
+                    // BUSCAR HOSPEDE ------------------------------
+                    System.out.println("Digite o nome do hospede que deseja verificar: ");
+                    teclado.nextLine();
+                    nomeHospede = teclado.nextLine();
 
-        // BUSCAR HOSPEDE ------------------------------
-        teclado.nextLine();
-        String nomeHospede;
-        System.out.println("Digite o nome do hospede que deseja verificar: ");
-        nomeHospede = teclado.nextLine();
+                    for (Quarto i : quartos) {
+                        if (i.getOcupado()) {
+                            if (i.getHospede().getNome().equals(nomeHospede)) {
+                                int numeroQuartoOcupado = i.getNumero();
+                                String telefone = i.getHospede().getTelefone();
+                                System.out.println("O mesmo se encontra hospedado no quarto: " + numeroQuartoOcupado
+                                        + ", telefone: " + telefone);
+                            }
+                        }
+                    }
+                    break;
 
-        for (Quarto i : quartos) {
-            if (i.getOcupado()) {
-                if (i.getHospede().getNome().equals(nomeHospede)) {
-                    int numeroQuartoOcupado = i.getNumero();
-                    String telefone = i.getHospede().getTelefone();
-                    System.out.println("O mesmo se encontra hospedado no quarto: " + numeroQuartoOcupado
-                            + ", telefone: " + telefone);
-                }
-            }
-        }
+                case 0:
+                    System.out.println("O faturamento total ficou em: R$" + faturamentoTotal);
 
-        // --------------------------------------------
-        // Falta somente criar o menu e a quebra de laço na execução
-        // Verificar os bugs do applicativo
+                    break;
+                
+                default:
+                    System.out.println("Opção invalida, selecione uma das opções anteriores.\n");
+                        
+            }   
 
+        } while (opcao != 0);
     }
+
 }
